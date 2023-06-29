3f4b6b4d13366c50c251751aa17cf689e83839e4
9000HAL <gsanchezscs@gmail.com>
2023-06-29 15:37:44 -0700

    whiteboard commit

---
+++ whiteboard_day4_week10.py
@@ -0,0 +1,32 @@
+
+
+#// Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+#// For example, a tower with 3 floors looks like this:
+
+#// [
+#//   "  *  ",
+#//   " *** ", 
+#//   "*****"
+#// ]
+
+
+
+#// A tower with 5 floors
+
+#// [
+#//   "    *    ",
+#//   "   ***   ",
+#//   "  *****  ",
+#//   " ******* ",
+#//   "*********",
+
+
+#// ]
+
+
+# create a function appends string to the list
+
+# first strin equal amount of white space
+
+# 
